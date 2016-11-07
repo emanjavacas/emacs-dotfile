a24ebdc905289510c846bb8c98cca26dc107133d
Enrique Manjavacas <enrique.manjavacas@gmail.com>
2016-11-07 10:21:44 +0100

    removed MacOS paths

--- init.el
+++ init.el
@@ -492,9 +492,9 @@ file of a buffer in an external program."
 ;; use Skim as default pdf viewer
 ;; Skim's displayline is used for forward search (from .tex to .pdf)
 ;; option -b highlights the current line; option -g opens Skim in the background  
-(setq TeX-view-program-selection '((output-pdf "PDF Viewer")))
-(setq TeX-view-program-list
-     '(("PDF Viewer" "/Applications/Skim.app/Contents/SharedSupport/displayline -b -g %n %o %b")))
+;; (setq TeX-view-program-selection '((output-pdf "PDF Viewer")))
+;; (setq TeX-view-program-list
+;;       '(("PDF Viewer" "/Applications/Skim.app/Contents/SharedSupport/displayline -b -g %n %o %b")))
 
 ;;; (helm)-BIBTEX
 (autoload 'helm-bibtex "helm-bibtex" "" t)

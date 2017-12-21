cf78f703edada312c5aa1714091709cbf656f11e
Enrique Manjavacas <enrique.manjavacas@gmail.com>
2017-12-21 19:19:07 +0100

    custom

--- init.el
+++ init.el
@@ -25,15 +25,42 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(csv-separators (quote (";" "	")))
+ '(csv-separators (quote (";" "	")) t)
  '(custom-safe-themes
    (quote
-    ("6a37be365d1d95fad2f4d185e51928c789ef7a4ccf17e7ca13ad63a8bf5b922f" default)))
- '(httpd-port 8082)
- '(js2-basic-offset 2)
- '(js2-bounce-indent-p nil)
+    ("40f6a7af0dfad67c0d4df2a1dd86175436d79fc69ea61614d668a635c2cd94ab" "6a37be365d1d95fad2f4d185e51928c789ef7a4ccf17e7ca13ad63a8bf5b922f" default)))
+ '(fci-rule-color "#383838" t)
+ '(httpd-port 8082 t)
+ '(js2-basic-offset 2 t)
+ '(js2-bounce-indent-p nil t)
+ '(nrepl-message-colors
+   (quote
+    ("#CC9393" "#DFAF8F" "#F0DFAF" "#7F9F7F" "#BFEBBF" "#93E0E3" "#94BFF3" "#DC8CC3")))
  '(org-latex-create-formula-image-program (quote imagemagick))
- '(org-latex-table-caption-above nil))
+ '(org-latex-table-caption-above nil t)
+ '(org-preview-latex-default-process (quote imagemagick))
+ '(vc-annotate-background "#2B2B2B" t)
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#BC8383")
+     (40 . "#CC9393")
+     (60 . "#DFAF8F")
+     (80 . "#D0BF8F")
+     (100 . "#E0CF9F")
+     (120 . "#F0DFAF")
+     (140 . "#5F7F5F")
+     (160 . "#7F9F7F")
+     (180 . "#8FB28F")
+     (200 . "#9FC59F")
+     (220 . "#AFD8AF")
+     (240 . "#BFEBBF")
+     (260 . "#93E0E3")
+     (280 . "#6CA0A3")
+     (300 . "#7CB8BB")
+     (320 . "#8CD0D3")
+     (340 . "#94BFF3")
+     (360 . "#DC8CC3"))) t)
+ '(vc-annotate-very-old-color "#DC8CC3" t))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
